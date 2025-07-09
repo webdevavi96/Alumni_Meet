2a365373f7e9f210bfba95807eb378f0f2cb844a
webdevavi96 <avinashchaurasiya902@gmail.com>
2025-07-09 19:55:47 +0530

    corrected requirements.txt

--- Alumni_Meet/settings.py
+++ Alumni_Meet/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "SECRET_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","mmitalumni.up.railway.app/").split(",")
 
 
 # Application definition
