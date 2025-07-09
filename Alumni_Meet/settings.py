9539e8cd40d66a318ed16e20fd3e0ca4838fdee1
webdevavi96 <avinashchaurasiya902@gmail.com>
2025-07-09 20:43:03 +0530

    corrected allowed hoists

--- Alumni_Meet/settings.py
+++ Alumni_Meet/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "SECRET_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = "ALLOWED_HOSTS"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 # Application definition
