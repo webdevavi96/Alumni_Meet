f44976854928aa1453c4ff7cd4f89f2b78657562
webdevavi96 <avinashchaurasiya902@gmail.com>
2025-11-16 09:03:34 +0530

    feat: Simplify welcome toast logic in Profile component

--- FrontEnd/src/pages/Profile/Profile.jsx
+++ FrontEnd/src/pages/Profile/Profile.jsx
@@ -47,14 +47,11 @@ function Profile() {
   };
 
   useEffect(() => {
-<<<<<<< HEAD
     if (user && !sessionStorage.getItem("toastShown")) {
       toast.info(`Welcome! ${user.fullName}`, { toastId: "welcome-toast" });
       sessionStorage.setItem("toastShown", "true");
     }
-=======
     if (user) toast.info(`Welcome! ${user.fullName}`);
->>>>>>> 6d56cc0b397cc48f83220cf90687f23bc9a1a138
   }, [])
 
 
